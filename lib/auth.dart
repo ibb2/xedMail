4c04b71f86b2f5efa92bcb861377de4417d999c8
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-06-16 12:02:20 +1000

    Google signin works. Exposed client secret.

---
+++ lib/auth.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart' hide GoogleSignIn;
+import 'package:google_sign_in_all_platforms/google_sign_in_all_platforms.dart';
+
+class SignInDemo extends StatefulWidget {
+  @override
+  _SignInDemoState createState() => _SignInDemoState();
+}
+
+class _SignInDemoState extends State<SignInDemo> {
+  late GoogleSignIn _googleSignIn;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn = GoogleSignIn(
+      params: GoogleSignInParams(
+        clientId:
+            '611007919856-2jae4hpql0eaeai46kpappvasajc1uf0.apps.googleusercontent.com',
+        clientSecret: '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',
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Google Sign-In Demo')),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () async {
+                final credentials = await _googleSignIn.signIn();
+                if (credentials != null) {
+                  print('Signed in successfully: ${credentials.accessToken}');
+                } else {
+                  print('Sign in failed');
+                }
+              },
+              child: const Text('Sign In'),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
